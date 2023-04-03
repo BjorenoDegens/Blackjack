4feb8b67d3d32bfb920b1fded07429b6b3f3d338
bjoreno <bjodegens@hotmail.com>
2023-04-03 13:03:53 +0200

    added classes

---
+++ Blackjack/Dealer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Class1
+    {
+
+        public void shuffle() 
+        {
+            
+        }
+        public void giveCard() 
+        {
+            
+        }
+    }
+}
---
+++ Blackjack/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Deck
+    {
+        public void shuffleDeck()
+        {
+            
+        }
+
+        public void nextCard()
+        {
+            
+        }
+    }
+}
